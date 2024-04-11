94055ce3444991fec3b45cff5f05671785184ed2
ObradICT <obrad.pualic.radujko.25.21@ict.edu.rs>
2024-04-11 07:30:56 -0700

    Mali up

--- proba.c
+++ proba.c
@@ -10,4 +10,7 @@ int main()
   printf("Ovo je promena za push iz teminala/ iz lokalnog gita");
 
   printf("nova promena"); # auuu vau
+
+
+  printf("Promena u kodu dok nije najnovija verzija main sa githuba, tj ovaj lokalni branch 1 commit kasni");
 }
